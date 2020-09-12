fa1d9355d68f95df7f763dca0147ca2f828b7ba8
Kartik <aggarwalkartik96@gmail.com>
2020-09-12 17:28:54 +0530

    Updates

---
+++ precache-manifest.0acbf3582bba5e2bf6ab7d6fe83e0105.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efea07f1c88db3d9011100128f0cccc0",
+    "url": "/spacexlaunches/index.html"
+  },
+  {
+    "revision": "b3a39dcead6633c127a5",
+    "url": "/spacexlaunches/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f6a09b30a1318a014a00",
+    "url": "/spacexlaunches/static/css/main.06fa56e1.chunk.css"
+  },
+  {
+    "revision": "b3a39dcead6633c127a5",
+    "url": "/spacexlaunches/static/js/2.276c8000.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/spacexlaunches/static/js/2.276c8000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a09b30a1318a014a00",
+    "url": "/spacexlaunches/static/js/main.0f107c07.chunk.js"
+  },
+  {
+    "revision": "1828d728ac59c6993d62",
+    "url": "/spacexlaunches/static/js/runtime-main.62f4c72b.js"
+  }
+]);
